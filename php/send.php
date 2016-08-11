af8e17224907438a868fe5fbc5f62e68802f0fcc
basskibo <jageticb@yahoo.com>
2016-08-11 15:51:46 +0200

    lots of stuf

--- php/send.php
+++ php/send.php
@@ -4,9 +4,9 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 $subject = $_POST['subject'];
 
-$to = 'hello@gmail.com';
+$to = 'jagetic.bojan@gmail.com';
 $message = 'FROM: '.$name.' Email: '.$email.'Message: '.$message;
-$headers = 'From: youremail@domain.com' . "\r\n";
+$headers = 'From: danisporta@domain.com' . "\r\n";
  
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // this line checks that we have a valid email address
 mail($to, $subject, $message, $headers); //This method sends the mail.
